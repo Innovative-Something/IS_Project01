42ff5c2c370fc277ab05eee6d80b3f544c3329f7
권혁준 <khjunejune10@gmail.com>
2022-01-18 14:11:04 +0900

    Android layout update.(+Lottie Animation)

--- app/src/main/java/com/example/is_project01/MainActivity.kt
+++ app/src/main/java/com/example/is_project01/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.is_project01
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.is_project01.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+
+        binding.writenBtn.setOnClickListener {
+            //todo 인텐트 -> 글쓰기 화면으로 넘어가기
+        }
     }
 }
---
+++ app/src/main/java/com/example/is_project01/SplashActivity.kt
@@ -0,0 +1,30 @@
+package com.example.is_project01
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed({
+            val intent = Intent(this, SignInActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
+            startActivity(intent)
+            finish()
+        }, Duration)
+
+
+    }
+
+    companion object {
+        private const val Duration: Long = 3000
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+    }
+}
